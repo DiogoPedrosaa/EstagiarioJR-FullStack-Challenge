20aa261152bcac9e1d8a5e08fac17f4a87ef45f1
Diogo de Melo Monte Pedrosa <diogo.pedrosa@financas.pref>
2025-02-11 12:57:26 -0300

    fix bug department not returning id number

--- maceio_in/serializers/department_serializers.py
+++ maceio_in/serializers/department_serializers.py
@@ -4,4 +4,4 @@ from maceio_in.models import Department
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = ['name', 'description']
+        fields = ['id', 'name', 'description']
